cb75fad81f8264acb9ce00d4c740de8ee6944ea5
Damon Romano <rromano@creditacceptance.com>
2024-07-02 00:07:11 -0400

    It's working!

--- vault.js
+++ vault.js
@@ -1,26 +1,10 @@
-console.log("Hello, World!");
+//Create a string to be shown to the user saying, "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
 
-//data types
+//Three variables. Each  one holds an arithmatic problem. The calculated value for each problem is one of the integers in the three-number combination to the vault.  
 
-/**
- * primitive types
- * 
- * string - character or seequence of characters wrapped in quotes (single or double quotes)
- * 
- * numbers - all kinds of numbers of all sizes that .js can handle, eg.- integers (whole numbers), signed numbers (positive or negative), decimal numbers
- * 
- * Boolean - true or false (without the quotes, eg.- "true" is not the same thing as true)
- * 
- * null - null - used to define soething as having no value at all. 
- * 
- * undefined - assigned as a value wherethings are undefined. 
- * 
- * reference -
- * 
- * Object - (object literals and arrays). 
- * 
- */
+const combo1 = 3 + 7;
+const combo2 = 5 * 8;
+const combo3 = 45 - 6;
 
-// variables - think of a variable as a "box" in which you can store data.
-
-let message = "Hello, World"; 
+alert(combo3);
