5ee5a5840412ac44e20fbaecf886f7e1ab2a8be3
Damon Romano <rromano@creditacceptance.com>
2024-07-01 21:35:37 -0400

    second commit, starting alerts for the vault combo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # block07
+
 .js intro
+For my Fullstack Academy bootcamp, this is the Block 07 Workshop exercise. It is an introduction to Javascript.
---
+++ vault.js
@@ -0,0 +1,26 @@
+console.log("Hello, World!");
+
+//data types
+
+/**
+ * primitive types
+ * 
+ * string - character or seequence of characters wrapped in quotes (single or double quotes)
+ * 
+ * numbers - all kinds of numbers of all sizes that .js can handle, eg.- integers (whole numbers), signed numbers (positive or negative), decimal numbers
+ * 
+ * Boolean - true or false (without the quotes, eg.- "true" is not the same thing as true)
+ * 
+ * null - null - used to define soething as having no value at all. 
+ * 
+ * undefined - assigned as a value wherethings are undefined. 
+ * 
+ * reference -
+ * 
+ * Object - (object literals and arrays). 
+ * 
+ */
+
+// variables - think of a variable as a "box" in which you can store data.
+
+let message = "Hello, World"; 
